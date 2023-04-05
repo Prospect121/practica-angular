8d508110d0d1e025dc1eadd59cc300e4e77db48e
Prospect321 <erick.nieto@zabud.com.co>
2023-04-04 21:50:15 -0500

    Se configura ambiente de prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  url: 'https://crudcrud.com/api/b2c629e398cc418b8f1f8737b846efd0/',
+  patch: {
+    unicorns: 'unicorns/',
+    user: 'user/',
+  },
+};
